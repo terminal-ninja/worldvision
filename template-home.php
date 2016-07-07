b6d1322dd25235d33e0ca97ecf04b54cc2ec44c2
terminal-ninja <stuart@ninjatech.marketing>
2016-07-07 20:30:17 +0200

    stu4

--- template-home.php
+++ template-home.php
@@ -67,45 +67,62 @@
       <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/" alt="">
       <h2>Health</h2>
       <p>Improving the health of women and the children by focusing on seven important health elements for mothers and 11 important health elements for children, thus contributing to the reduction of under-five and maternal mortality.</p>
-    </article>
-    <article>
-      <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/" alt="">
-      <h2>Health</h2>
-      <p>Improving the health of women and the children by focusing on seven important health elements for mothers and 11 important health elements for children, thus contributing to the reduction of under-five and maternal mortality.</p>
-    </article>
-    <article>
-      <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/" alt="">
-      <h2>Health</h2>
-      <p>Improving the health of women and the children by focusing on seven important health elements for mothers and 11 important health elements for children, thus contributing to the reduction of under-five and maternal mortality.</p>
-    </article>
-    <article>
-      <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/" alt="">
-      <h2>Health</h2>
-      <p>Improving the health of women and the children by focusing on seven important health elements for mothers and 11 important health elements for children, thus contributing to the reduction of under-five and maternal mortality.</p>
-    </article>
-    <article>
-      <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/" alt="">
-      <h2>Health</h2>
-      <p>Improving the health of women and the children by focusing on seven important health elements for mothers and 11 important health elements for children, thus contributing to the reduction of under-five and maternal mortality.</p>
-    </article>
-  </section>
-  <section>
-    <article>
-      <h2>About World Vision</h2>
-      <p>World Vision is a Christian humanitarian organization dedicated to working with children, families, and their communities worldwide to reach their full potential by tackling the root causes of poverty and injustice. Working in nearly 100 countries around the world, we serve all people, regardless of religion, race, ethnicity, or gender.
-      </p>
-      <div class="item">
-        <div class="button">
-          <a href="#" class="primary">LEARN</a>
-        </div>
-        <div class="button">
-          <a href="#" class="primary">DONATE</a>
-        </div>
-        <div class="button">
-          <a href="#" class="primary">SPONSOR</a>
-        </div>
+      <div class="button">
+        <a href="#" class="secondary">DONATE</a>
       </div>
     </article>
+    <article>
+      <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/" alt="">
+      <h2>Health</h2>
+      <p>Improving the health of women and the children by focusing on seven important health elements for mothers and 11 important health elements for children, thus contributing to the reduction of under-five and maternal mortality.</p>
+      <div class="button">
+        <a href="#" class="secondary">DONATE</a>
+      </div>
+    </article>
+    <article>
+      <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/" alt="">
+      <h2>Health</h2>
+      <p>Improving the health of women and the children by focusing on seven important health elements for mothers and 11 important health elements for children, thus contributing to the reduction of under-five and maternal mortality.</p>
+      <div class="button">
+        <a href="#" class="secondary">DONATE</a>
+      </div>
+    </article>
+    <article>
+      <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/" alt="">
+      <h2>Health</h2>
+      <p>Improving the health of women and the children by focusing on seven important health elements for mothers and 11 important health elements for children, thus contributing to the reduction of under-five and maternal mortality.</p>
+      <div class="button">
+        <a href="#" class="secondary">DONATE</a>
+      </div>
+    </article>
+    <article>
+      <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/" alt="">
+      <h2>Health</h2>
+      <p>Improving the health of women and the children by focusing on seven important health elements for mothers and 11 important health elements for children, thus contributing to the reduction of under-five and maternal mortality.</p>
+      <div class="button">
+        <a href="#" class="secondary">DONATE</a>
+      </div>
+    </article>
+  </section>
+  <section class="about">
+    <div class="container">
+      <article>
+        <h2 class="white">About World Vision</h2>
+        <p class="white">World Vision is a Christian humanitarian organization dedicated to working with children, families, and their communities worldwide to reach their full potential by tackling the root causes of poverty and injustice. Working in nearly 100 countries around the world, we serve all people, regardless of religion, race, ethnicity, or gender.
+        </p>
+        <div class="item">
+          <div class="button">
+            <a href="#" class="primary primary-inv">LEARN</a>
+          </div>
+          <div class="button">
+            <a href="#" class="primary primary-inv">DONATE</a>
+          </div>
+          <div class="button">
+            <a href="#" class="primary primary-inv">SPONSOR</a>
+          </div>
+        </div>
+      </article>
+    </div>
   </section>
 
   <!-- <section class="content services">
