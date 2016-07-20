128440db62940d9775df150356eee9c97a3daf8d
terminal-ninja <stuart@ninjatech.marketing>
2016-07-20 09:47:56 +0200

    Leadership

--- template-leadership.php
+++ template-leadership.php
@@ -2,83 +2,100 @@
 
   <h2 class="heading">Our Leadership</h2>
 
-  <section>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur omnis, minima ea praesentium nam amet aliquid ratione recusandae facilis eos nihil fugit in numquam iusto voluptas officia odit, totam repudiandae!</p>
+  <section class="center no-margin">
+    <p class="black">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur omnis, minima ea praesentium nam amet aliquid ratione recusandae facilis eos nihil fugit in numquam iusto voluptas officia odit, totam repudiandae!</p>
   </section>
-
-  <section>
-    <h2 class="grey-heading">Executive Team</h2>
+  <div class="container center">
+    <h2 class="heading-grey">Executive Team</h2>
+  </div>
+  <section class="executives no-margin">
     <div class="item">
       <figure>
-        <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/icons/heart.png" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/dr-rudzani-muloiwa.jpg" alt="">
       </figure>
-      <h3>Paula Barnard</h3>
-      <h6>CEO</h6>
-      <div class="button">
-        <a href="#" class="secondary">VIEW</a>
+      <div class="inner">
+        <h3>Paula Barnard</h3>
+        <h6>CEO</h6>
+      </div>
+      <div class="button full-btn">
+        <a href="#" class="secondary">View</a>
       </div>
     </div>
     <div class="item">
       <figure>
-        <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/icons/heart.png" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/dr-rudzani-muloiwa.jpg" alt="">
       </figure>
-      <h3>Paula Barnard</h3>
-      <h6>CEO</h6>
-      <div class="button">
-        <a href="#" class="secondary">VIEW</a>
+      <div class="inner">
+        <h3>Paula Barnard</h3>
+        <h6>CEO</h6>
+      </div>
+      <div class="button full-btn">
+        <a href="#" class="secondary">View</a>
       </div>
     </div>
     <div class="item">
       <figure>
-        <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/icons/heart.png" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/dr-rudzani-muloiwa.jpg" alt="">
       </figure>
-      <h3>Paula Barnard</h3>
-      <h6>CEO</h6>
-      <div class="button">
-        <a href="#" class="secondary">VIEW</a>
+      <div class="inner">
+        <h3>Paula Barnard</h3>
+        <h6>CEO</h6>
+      </div>
+      <div class="button full-btn">
+        <a href="#" class="secondary">View</a>
       </div>
     </div>
   </section>
-  <section>
-    <h2 class="grey-heading">Executive Team</h2>
+  <section class="container center">
+    <h2 class="heading-grey">Our Board</h2>
+  </section>
+  <section class="board">
     <div class="item">
       <figure>
-        <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/icons/heart.png" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/dr-rudzani-muloiwa.jpg" alt="">
       </figure>
-      <h3>Paula Barnard</h3>
-      <h6>CEO</h6>
-      <div class="button">
-        <a href="#" class="secondary">VIEW</a>
+      <div class="inner">
+        <h3>Paula Barnard</h3>
+        <h6>CEO</h6>
+      </div>
+      <div class="button full-btn">
+        <a href="#" class="secondary">View</a>
       </div>
     </div>
     <div class="item">
       <figure>
-        <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/icons/heart.png" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/dr-rudzani-muloiwa.jpg" alt="">
       </figure>
-      <h3>Paula Barnard</h3>
-      <h6>CEO</h6>
-      <div class="button">
-        <a href="#" class="secondary">VIEW</a>
+      <div class="inner">
+        <h3>Paula Barnard</h3>
+        <h6>CEO</h6>
+      </div>
+      <div class="button full-btn">
+        <a href="#" class="secondary">View</a>
       </div>
     </div>
     <div class="item">
       <figure>
-        <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/icons/heart.png" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/dr-rudzani-muloiwa.jpg" alt="">
       </figure>
-      <h3>Paula Barnard</h3>
-      <h6>CEO</h6>
-      <div class="button">
-        <a href="#" class="secondary">VIEW</a>
+      <div class="inner">
+        <h3>Paula Barnard</h3>
+        <h6>CEO</h6>
+      </div>
+      <div class="button full-btn">
+        <a href="#" class="secondary">View</a>
       </div>
     </div>
     <div class="item">
       <figure>
-        <img class="heading" src="<?php echo get_template_directory_uri(); ?>/img/icons/heart.png" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/dr-rudzani-muloiwa.jpg" alt="">
       </figure>
-      <h3>Paula Barnard</h3>
-      <h6>CEO</h6>
-      <div class="button">
-        <a href="#" class="secondary">VIEW</a>
+      <div class="inner">
+        <h3>Paula Barnard</h3>
+        <h6>CEO</h6>
+      </div>
+      <div class="button full-btn">
+        <a href="#" class="secondary">View</a>
       </div>
     </div>
   </section>
